4a09d932a32b019bc1ccbc71deaddede7db33b42
howitphils <uladzislauhr@gmail.com>
2026-01-21 09:57:38 +0300

    Removed testing folder

--- __tests__/blogs.test.ts
+++
@@ -1,8 +0,0 @@
-import { req } from "./test-helper";
-import { describe, it } from "@jest/globals";
-
-describe("Blogs API", () => {
-  it("should return hello world", async () => {
-    await req.get("/").expect(200, "Hello, World!");
-  });
-});
