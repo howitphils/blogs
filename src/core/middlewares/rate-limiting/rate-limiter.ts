5e2c623c23ae7e9f75207717383bbf2fddee4636
howitphils <uladzislauhr@gmail.com>
2026-02-20 12:45:02 +0300

    added ip white list for rate limiting

--- src/core/middlewares/rate-limiting/rate-limiter.ts
+++ src/core/middlewares/rate-limiting/rate-limiter.ts
@@ -2,9 +2,14 @@ import rateLimit from "express-rate-limit";
 import { HttpStatus } from "../../types/http-status-types";
 import { appSettings } from "../../../app-settings";
 
+const whitelist = ["127.0.0.1", "::1", "::ffff:127.0.0.1"];
+
 export const rateLimiter = rateLimit({
   windowMs: appSettings.rateLimit.windowMs,
   limit: appSettings.rateLimit.requestLimit,
   message: appSettings.rateLimit.errorMessage,
   statusCode: HttpStatus.TOO_MANY_REQUESTS,
+  skip: (req) => {
+    return whitelist.includes(req.ip as string);
+  },
 });
