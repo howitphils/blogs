c41afac8a51ef620d704146759d0637682dba3ba
howitphils <uladzislauhr@gmail.com>
2026-03-02 09:58:32 +0300

    removed skip from rate limiter

--- src/core/middlewares/rate-limiting/rate-limiter.ts
+++ src/core/middlewares/rate-limiting/rate-limiter.ts
@@ -4,8 +4,6 @@ import { appSettings } from "../../../app-settings";
 import RedisStore from "rate-limit-redis";
 import { redisClient } from "../../../db/redis";
 
-// const whitelist = ["127.0.0.1", "::1", "::ffff:127.0.0.1"];
-
 export const rateLimiter = rateLimit({
   windowMs: appSettings.rateLimit.windowMs,
   limit: appSettings.rateLimit.requestLimit,
@@ -13,12 +11,5 @@ export const rateLimiter = rateLimit({
   statusCode: HttpStatus.TOO_MANY_REQUESTS,
   store: new RedisStore({
     sendCommand: (...args) => redisClient.sendCommand(args),
-    prefix: "rl:",
   }),
-  // skip: (req) => {
-  //   return whitelist.includes(req.ip as string);
-  // },
 });
-
-//TODO: зачистка ключей в редис перед каждым тестом (добавить отдельную редис бд - пока невозможно без env.test файла)
-// + убрать скип
